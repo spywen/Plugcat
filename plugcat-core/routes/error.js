9292d1722572fa49a58c561a59ff6fc219c3995f
Laurent <spywen@hotmail.fr>
2015-10-10 14:17:20 +0200

    More information when server error

--- plugcat-core/routes/error.js
+++ plugcat-core/routes/error.js
@@ -1,7 +1,7 @@
 var logger = require('./../helpers/logger');
 
 exports.errorHandler = function(err, req, res, next) {
-    logger.error('Error 500 - server error');
+    logger.error('Error 500 - server error' + JSON.stringify(err));
     res.status(500);
     res.render('500', { error: err });
 };
